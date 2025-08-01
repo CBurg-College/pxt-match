e23debab94b7c73a9b8c56b4e504034daa2488ce
CBurg-College <cburgmicro@outlook.com>
2025-08-01 11:48:43 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,18 +1,30 @@
 /* This is a basic extension for robot matches.
  * The robot-players and arbiter must include it
  * to be able to send and respond to 'Match'-messages.
+ * 
  * In the current extension the radio event handler
  * 'onReceivedNumber' stores the message in variable
  * 'MATCH' and then calls a 'matchHandler' of which
- * the code should be registered by the players and
- * the arbiter through calling 'setMatchHandling' as
- * follows:     setMatchHandling(() => {
+ * the code should be registered by the depending
+ * arbiter and player extensions through calling
+ * 'setMatchHandling' as follows:
+ *              setMatchHandling(() => {
  *                  ... handling code ...
  *              })
  * The handling code should respond to the message
  * stored in the variable MATCH.
+ * 
+ * Additionally, the depending player extensions
+ * (e.g. pxt-soccer-player) should implement a function
+ * that allows to create code for the 'playerHandler',
+ * e.g.:        //% color="#FFCC00"
+ *              //% block="when playing"
+ *              //% block.loc.nl="gedurende het spel"
+ *              export function onEventOutsideField(
+ *                                  programmableCode: () => void): void {
+ *                  playerHandler = programmableCode;
+ *              }
  */
-
 
 enum Match {
     Stop,
