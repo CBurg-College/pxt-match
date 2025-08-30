f49b94d012a6d7e9d8fa9a7dbcf50bfe7b591f29
CBurg-College <cburgmicro@outlook.com>
2025-08-30 22:38:01 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,12 +3,9 @@
  * it to be able to send and respond to 'Match'-messages.
  * 
  * The radio event handler 'onReceivedNumber' stores
- * the messages in variable 'MATCH'. This variable is
- * repeatedly (forever) handled in a state machine.
- * According to the value in variable MATCH, the state
- * machine calls handlers that must be registered by the
- * extensions which are based on the pxt-match extension.
- * For example, registering goes like:
+ * the messages in variable 'MATCH' and calls the
+ * accompanying handler. Dependent extensions should
+ * register the handlers, for example:
  *   //% block="when playing do"
  *   export function onPlay(code: () => void) : void   {
  *       playHandler = code;
@@ -18,9 +15,8 @@
  * register the playHandler.
  * 
  * IMPORTANT NOTE:
- * Extensions that are based in the pxt-match extension
- * should have the following line of code as the first
- * line of EACH loop:
+ * Dependent extensions should have the following line
+ * of code as the first line of EACH loop:
  *   if (MATCH != Match.Play) return;
  * This ensures a quick response to the messages.
  */
@@ -58,28 +54,22 @@ let winnerHandler: handler
 let loserHandler: handler
 let disqualHandler: handler
 
-//
-// game state machine
-//
 basic.forever(function() {
+    if ((MATCH == Match.Play) && playHandler)
+        playHandler()
+})
+
+radio.onReceivedNumber(function (match: number) {
+    MATCH = match
     switch (MATCH) {
-        case Match.Play:
-            if (playHandler) {
-                if (ROLE == Role.PlayerYellow || ROLE == Role.PlayerBlue)
-                    playHandler()
-            }
-            break
         case Match.PointYellow:
-            if (pointHandler && (ROLE == Role.GoalYellow))
-                pointHandler()
-            MATCH = Match.Stop
-            if (resetHandler) resetHandler()
-            break
         case Match.PointBlue:
-            if (pointHandler && (ROLE == Role.GoalBlue))
-                pointHandler()
-            MATCH = Match.Stop
-            if (resetHandler) resetHandler()
+            if (ROLE == Role.Arbiter) {
+                if (pointHandler) pointHandler()
+            }
+            else {
+                if (resetHandler) resetHandler()
+            }
             break
         case Match.DisallowYellow:
             if (disallowHandler && (ROLE == Role.GoalYellow))
@@ -96,7 +86,6 @@ basic.forever(function() {
                 winnerHandler()
             if (loserHandler && (ROLE == Role.PlayerBlue))
                 loserHandler()
-            MATCH = Match.Stop
             if (resetHandler) resetHandler()
             break
         case Match.WinnerBlue:
@@ -104,34 +93,18 @@ basic.forever(function() {
                 winnerHandler()
             if (loserHandler && (ROLE == Role.PlayerYellow))
                 loserHandler()
-            MATCH = Match.Stop
             if (resetHandler) resetHandler()
             break
         case Match.DisqualYellow:
             if (disqualHandler && (ROLE == Role.PlayerYellow))
                 disqualHandler()
-            MATCH = Match.Stop
             if (resetHandler) resetHandler()
             break
         case Match.DisqualBlue:
             if (disqualHandler && (ROLE == Role.PlayerBlue))
                 disqualHandler()
-            MATCH = Match.Stop
             if (resetHandler) resetHandler()
             break
     }
     OLDMATCH = MATCH
-})
-
-function active() : boolean {
-    return (MATCH == Match.Play)
-}
-
-function inactive() : boolean {
-    return (MATCH != Match.Play)
-}
-
-radio.onReceivedNumber(function (match: number) {
-    MATCH = match
-    basic.showNumber(MATCH)
 })
